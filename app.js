66a0123ac8469637414a43cb8db24b6344dbd301
Ahmed-517 <ahmed195041@feng.bu.edu.eg>
2022-09-21 01:38:26 +0200

    commit

--- app.js
+++ app.js
@@ -1,16 +1,65 @@
+const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
 app = express();
 
-const port = 2517;
+// 1) MIDLLEWARES
+app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Hello from the other side',
-        app: 'Natours'
-    })
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('Hello from the middleware 🙋‍♂️');
+    next();
 })
 
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
 })
+
+
+
+
+// 2) ROUTE HANDLERS
+
+
+
+
+// 3) ROUTES
+
+/*
+app.get('/api/v1/tours', getAllTours)
+app.get('/api/v1/tours/:id', getTour)
+app.post('/api/v1/tours', createTour)
+app.patch('/api/v1/tours/:id', updateTour)
+app.delete('/api/v1/tours/:id', deleteTour)
+*/
+
+
+
+
+
+
+
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
+// 4) START THE SERVER
+
+
+
+
+// Important Points
+
+// we can add a vriable using :
+
+// PUT vs PATCH
+// PUT: we expect that our app receives the entire new updated object
+// PATCH: we only expect the properties that should actually be updated on the object
+// so PATCH is better 
+
+module.exports = app;
